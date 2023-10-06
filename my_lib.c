b6ac3ded1acd488060f7305107924c22bb271d77
IvanSpallaUgalde <ivanuib332@gmail.com>
2023-10-06 11:41:42 +0200

    my_strcat y my_strchr

--- my_lib.c
+++ my_lib.c
@@ -23,3 +23,66 @@ int my_strcmp(const char *str1, const char *str2){
     }
     return (str1[i] - str2[j]);
 }
+
+char my_strcat(char *dest, const char *str){
+
+    //Tamaño es igual al num de char de str + num de char de dest + 1 (caracter nulo)
+    size_t tam = sizeof(char) * (my_strlen(str) + my_strlen(dest) + 1);
+    int i = 0; //Contador
+
+    char *a = (char *)malloc(tam);
+
+    while (*dest)
+    {
+        *a = *dest;
+        dest++;
+        a++;
+    }
+
+    while (*str)
+    {
+        *a = *str;
+        str++;
+        a++;
+    }
+    
+    *a = '\0';
+    
+    while (i > 0)
+    {
+        a--;
+        i--;
+    }
+
+    dest = a;
+
+    //Debug
+    printf("Contenido de dest: %s\n", dest);
+
+    return (char *)dest;
+}
+
+char my_strchr(const char *str, int c){
+    
+    //Mientras str =! 0
+    while (*str)
+    {
+        if (*str == c)
+        {
+            //Casting y return del caracter de str
+            return (char *)str;
+        }
+        
+        //Siguiente caracter de la cadena str
+        str++;
+    }
+    
+}
+
+int main(){
+    char dest[] = "Hola ";
+    char str[] = "mundo";
+
+    my_strcat(dest, str);
+    return 0;
+}
