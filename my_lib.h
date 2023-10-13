19ce485be655eb96f37f6741681e36e04a74d29a
EliteEmperorTK <147103748+EliteEmperorTK@users.noreply.github.com>
2023-10-13 15:55:18 +0200

    Update my_lib.h

--- my_lib.h
+++ my_lib.h
@@ -40,4 +40,3 @@ int my_stack_len(struct my_stack *stack);
 int my_stack_purge(struct my_stack *stack); 
 struct my_stack *my_stack_read(char *filename);
 int my_stack_write(struct my_stack *stack, char *filename);
-    
