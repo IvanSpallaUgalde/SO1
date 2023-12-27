4d17ac8b2646bf9624f499e15fcb35e2f2d5acee
Marc <parasitohumanitario@gmail.com>
2023-12-27 12:33:07 +0100

    Puntos 2, 3, 4 y 5 implementados

--- SO1_P3/reader.c
+++ SO1_P3/reader.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include "my_lib.h"
 
+int main() {
+    my_stack_read();
+    int stack[STACK_SIZE];
+
+    //Inicializamos las variables
+    int suma = 0;
+    int minimo = INT_MAX;
+    int maximo = INT_MIN;
+    int contador = 0;
+
+    //Imprimimos los valores de la pila y realizamos cálculos
+    for (int i = 0; i < STACK_SIZE; ++i) {
+        printf("Valor %d: %d\n", i + 1, stack[i]);
+        suma += stack[i];
+        if (stack[i] < minimo) {
+            minimo = stack[i];
+        }
+        if (stack[i] > maximo) {
+            maximo = stack[i];
+        }
+        contador++;
+    }
+
+    //Imprimimos los resultados de los cálculos
+    printf("Suma: %d\n", suma);
+    printf("Mínimo: %d\n", minimo);
+    printf("Máximo: %d\n", maximo);
+    printf("Media: %.2f\n", (float)suma / contador);
+
+    return 0;
+}
--- SO1_P3/stack_counters.c
+++ SO1_P3/stack_counters.c
@@ -1,22 +1,16 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "my_lib.h"
 
 #define MAX_THREADS 10
 #define STACK_SIZE 10
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////CREO QUE NO HACE FALTA ESTA PARTE, HAY QUE HACER ALGO PARA USAR FUNCIONES DE OTRO FICHERO, PERO NO SÉ HACERLO////////////////
-void my_stack_init();
-int my_stack_len();
-void my_stack_push(int *data);
-int *my_stack_pop();
-void my_stack_purge();
-void my_stack_read(FILE *file);
-void my_stack_write(FILE *file);
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int *stack[STACK_SIZE];                             //Pila
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;  //Semáforo
 
-// Función para crear o verificar la pila
+
+//Función para crear o verificar la pila
 void prepare_stack(char *filename) {
     // Verificar si se proporcionó un nombre de archivo
     if (filename == NULL) {
@@ -24,33 +18,33 @@ void prepare_stack(char *filename) {
         return;
     }
 
-    // Verificar si la pila existe en el archivo
+    //Verificar si la pila existe en el archivo
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
-        // Si no existe, crearla e inicializarla
-        my_stack_init();
+        //Si no existe, crearla e inicializarla
+        my_stack_init();                                ////////////////¿Aquí no falta que una pila reciba el resultado de my_stack_init()?
         printf("Pila creada y inicializada.\n");
         file = fopen(filename, "w");
         if (file == NULL) {
             printf("Error al abrir el archivo para escritura.\n");
             return;
         }
-        // Agregar punteros a cero en la pila hasta llegar a STACK_SIZE
+        //Agregar punteros a cero en la pila hasta llegar a STACK_SIZE
         for (int i = 0; i < STACK_SIZE; ++i) {
-            int *data = (int *)malloc(sizeof(int)); // Reservar espacio para el entero
+            int *data = (int *)malloc(sizeof(int)); //Reservar espacio para el entero
             *data = 0; // Inicializar a cero
             my_stack_push(data);
-            fprintf(file, "%p\n", (void *)data); // Guardar la dirección en el archivo
+            fprintf(file, "%p\n", (void *)data); //Guardar la dirección en el archivo
         }
         fclose(file);
         printf("Pila inicializada con punteros a cero.\n");
     } else {
-        // Si existe, leer la pila y verificar su tamaño
+        //Si existe, leer la pila y verificar su tamaño
         my_stack_read(file);
         fclose(file);
         int stack_len = my_stack_len();
         if (stack_len < STACK_SIZE) {
-            // Agregar punteros a cero hasta STACK_SIZE
+            //Agregar punteros a cero hasta STACK_SIZE
             file = fopen(filename, "a");
             if (file == NULL) {
                 printf("Error al abrir el archivo para escritura.\n");
@@ -74,6 +68,34 @@ void prepare_stack(char *filename) {
 
 
 
+//Función para crear los hilos
+void create_threads(pthread_t *threads) {
+    for (int i = 0; i < MAX_THREADS; ++i) {
+        if (pthread_create(&threads[i], NULL, NULL, NULL) != 0) {
+            perror("Error al crear el hilo");  /////////////////////////////////MANEJAR ERRORES BIEN!!!!/////////////////        
+        }
+    }
+}
+
+//Función para ejecutar los hilos
+void *worker(void *ptr) {
+    for (int i = 0; i < 1000000; ++i) {
+        pthread_mutex_lock(&mutex);
+        int *data = my_stack_pop();
+        if (data == NULL) {
+            fprintf(stderr, "Error al extraer dato de la pila\n");
+            exit(EXIT_FAILURE);
+        }
+        (*data)++;
+        my_stack_push(data);
+        pthread_mutex_unlock(&mutex);
+    }
+    pthread_exit(NULL);
+}
+
+
+
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("Error: Sintaxis incorrecta. Uso: ./stack_counters <nombre_fichero>\n");
@@ -82,6 +104,26 @@ int main(int argc, char *argv[]) {
 
     char *filename = argv[1];
     prepare_stack(filename);
+
+    pthread_t threads[MAX_THREADS]; //Array para almacenar el PID de los hilos
+    create_threads(threads);        //Creamos los hilos
+
+
+    for (int i = 0; i < MAX_THREADS; ++i) {  //Esperamos a que acaben los hilos
+        if (pthread_join(threads[i], NULL) != 0) {
+            perror("Error al esperar a que el hilo termine");
+                                                            //////////////////////////////GESTIÓN DE ERRORES!!!///////////////////
+        }
+    }
+
+    //Volcam la pila en el fichero
+    my_stack_write();
+
+    //Liberar el espacio de la pila de la memoria principal
+    my_stack_purge();
+
+    // Salir del hilo principal
+    pthread_exit(NULL);
 
     return 0;
 }
